4b416366ac2930e1c43dcaa5c8a02788428adac3
jontama <whsxkak@gmail.com>
2015-06-29 00:21:28 +0900

    그냥

---
+++ src/test/TestAbstact.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * Created by jin on 2015. 6. 28..
+ */
+public abstract class TestAbstact {
+    public void aaa(){
+        int a = 10;
+        int b = 20;
+        int c = 30;
+    }
+
+    abstract void calc();
+}
---
+++ src/test/TestMain.java
@@ -0,0 +1,13 @@
+package test;
+
+/**
+ * Created by jin on 2015. 6. 29..
+ */
+public class TestMain extends TestAbstact {
+
+
+    @Override
+    void calc() {
+
+    }
+}
